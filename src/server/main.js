864732993c222c4f6c5c2a7dacead4c35de3a5c8
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-09-13 09:56:20 +0800

    Updated main.js.

--- src/server/main.js
+++ src/server/main.js
@@ -1,10 +1,12 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import router from "./routes.js";
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", router);
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
