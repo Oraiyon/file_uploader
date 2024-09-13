55b6311e24eb65f73b88c1615270634ebb6ff0a2
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-09-13 12:03:21 +0800

    Added passport

--- src/server/main.js
+++ src/server/main.js
@@ -1,11 +1,30 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import passport from "passport";
+import session from "express-session";
+import dotenv from "dotenv";
 import router from "./routes.js";
+import initializePassport from "./passport_config.js";
 
 const app = express();
 
+dotenv.config();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24
+    }
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+initializePassport(passport);
 
 app.use("/", router);
 
