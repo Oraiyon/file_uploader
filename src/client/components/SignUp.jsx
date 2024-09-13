cdb0a94f8acdc614d6dc067b0446c295d65ac623
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-09-13 12:36:28 +0800

    Created SignUp.jsx. Renders sign up form.

---
+++ src/client/components/SignUp.jsx
@@ -0,0 +1,17 @@
+import styles from "../stylesheets/SignUp.module.css";
+
+const SignUp = () => {
+  return (
+    <>
+      <form action="" method="post">
+        <label htmlFor="username">Username:</label>
+        <input type="text" name="username" id="username" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" id="password" />
+        <button>Sign Up</button>
+      </form>
+    </>
+  );
+};
+
+export default SignUp;
--- src/client/router.jsx
+++ src/client/router.jsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./components/App";
+import SignUp from "./components/SignUp";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <App />,
+      element: <App />
     },
+    {
+      path: "/Signup",
+      element: <SignUp />
+    }
   ]);
 
   return <RouterProvider router={router} />;
