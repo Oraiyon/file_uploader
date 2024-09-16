36fedd1775f37c3a9399e8df4322263703a9c202
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-09-16 14:44:57 +0800

    Added links for convenience

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 const App = () => {
-  return <h1>HELLO WORLD</h1>;
+  return (
+    <>
+      <h1>HELLO WORLD</h1>
+      <Link to={"/signup"}>Sign Up</Link>
+      <Link to={"/login"}>Log In</Link>
+    </>
+  );
 };
 
 export default App;
