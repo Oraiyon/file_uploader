3af55698e518b57e0dceea66eab3409e735bf54a
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-09-16 10:44:14 +0800

    Refactored SignUp.jsx. Removed unecessary onSubmit fucntion

--- src/client/components/SignUp.jsx
+++ src/client/components/SignUp.jsx
@@ -1,47 +1,16 @@
-import { useRef } from "react";
 import styles from "../stylesheets/SignUp.module.css";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  const usernameRef = useRef(null);
-  const passwordRef = useRef(null);
-  const confirmPasswordRef = useRef(null);
-
-  const submitSignUp = async (e) => {
-    try {
-      e.preventDefault();
-      const fetchData = await fetch("/api/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          username: usernameRef.current.value,
-          password: passwordRef.current.value,
-          confirmPassword: confirmPasswordRef.current.value
-        })
-      });
-      const data = await fetchData.json();
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <>
-      <form action="" onSubmit={submitSignUp}>
+      <form action="" method="POST">
         <label htmlFor="username">Username:</label>
-        <input type="text" name="username" id="username" ref={usernameRef} />
+        <input type="text" name="username" id="username" />
         <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="password" ref={passwordRef} />
+        <input type="password" name="password" id="password" />
         <label htmlFor="confirmPassword">Confirm Password:</label>
-        <input
-          type="password"
-          name="confirmPassword"
-          id="confirmPassword"
-          ref={confirmPasswordRef}
-        />
+        <input type="password" name="confirmPassword" id="confirmPassword" />
         <button>Sign Up</button>
       </form>
       <Link to={"/login"}>Login</Link>
