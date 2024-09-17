67eecbd7a1235dc559895804a443e5ee5f1f32cf
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-09-17 13:17:04 +0800

    Moved useEffect() to get user data to App.jsx

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,11 +1,41 @@
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    try {
+      const getUser = async () => {
+        if (window.location.pathname !== "/") {
+          const fetchUser = await fetch(`/api${window.location.pathname}`);
+          const data = await fetchUser.json();
+          if (!data) {
+            window.location.href = "/login";
+            return;
+          }
+          setUser(data);
+        }
+      };
+      getUser();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const DisplayPage = () => {
+    if (user) {
+      return <Outlet />;
+    } else {
+      return <h2>APP PAGE</h2>;
+    }
+  };
+
   return (
     <>
-      <Header />
-      <h2>APP PAGE</h2>
+      <Header user={user} />
+      <DisplayPage />
     </>
   );
 };
--- src/client/components/User.jsx
+++ src/client/components/User.jsx
@@ -1,29 +1,6 @@
-import { useEffect, useState } from "react";
-import Header from "./Header";
-
 const User = () => {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    try {
-      const getUser = async () => {
-        const fetchUser = await fetch(`/api${window.location.pathname}`);
-        const data = await fetchUser.json();
-        if (!data) {
-          window.location.href = "/login";
-          return;
-        }
-        setUser(data);
-      };
-      getUser();
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-
   return (
     <>
-      <Header user={user} />
       <h2>USER PAGE</h2>
     </>
   );
