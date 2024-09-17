941c27d217f877689f45b15eb84317557765589d
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-09-17 12:55:08 +0800

    Created & styled .header

---
+++ src/client/components/Header.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import styles from "../stylesheets/Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <nav className={styles.header}>
+        <h1>Hello {props.user ? props.user.username : "World"}</h1>
+        {props.user ? (
+          <a href="/logout">Log Out</a>
+        ) : (
+          <div className={styles.header_links}>
+            <Link to={"/signup"}>Sign Up</Link>
+            <Link to={"/login"}>Log In</Link>
+          </div>
+        )}
+      </nav>
+    </>
+  );
+};
+
+export default Header;
