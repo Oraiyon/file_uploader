03a06cffc4891b2e9b311e6695ca43d010da5aac
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-09-17 14:10:05 +0800

    Removed returning <DisplayPage />

--- src/client/components/User.jsx
+++ src/client/components/User.jsx
@@ -3,19 +3,11 @@ import { useOutletContext } from "react-router-dom";
 const User = () => {
   const [user] = useOutletContext();
 
-  const DisplayPage = () => {
-    if (!user) {
-      return <h2>APP PAGE</h2>;
-    } else {
-      return <h2>USER PAGE</h2>;
-    }
-  };
-
-  return (
-    <>
-      <DisplayPage />
-    </>
-  );
+  if (!user) {
+    return <h2>APP PAGE</h2>;
+  } else {
+    return <h2>USER PAGE</h2>;
+  }
 };
 
 export default User;
