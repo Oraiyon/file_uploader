d4cb1671273b0a2089fb74868d7a7cd0dbc010bd
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-10-04 14:31:23 +0800

    Added download & delete functions to Folder.jsx. Removed <FileButtons />

--- src/client/components/Folder.jsx
+++ src/client/components/Folder.jsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { Link, useOutletContext } from "react-router-dom";
 import styles from "../stylesheets/Folder.module.css";
 import Navbar from "./Navbar";
-import FileButtons from "./FileButtons";
 
 const Folder = () => {
   const [
@@ -34,6 +33,22 @@ const Folder = () => {
     return;
   }
 
+  const downloadFile = (file) => {
+    const start = file.url.substr(0, 50);
+    const end = file.url.slice(49);
+    const url = start + `fl_attachment:${file.name}` + end;
+    downloadLink.current.href = url;
+    downloadLink.current.click();
+  };
+
+  const deleteFile = async (file) => {
+    const response = await fetch(`/api/${user.id}/${selectedFolder.id}/delete/${file.id}`, {
+      method: "DELETE"
+    });
+    const data = await response.json();
+    setFiles(data);
+  };
+
   return (
     <>
       <Navbar level={2} user={user} selectedFolder={selectedFolder} />
@@ -41,20 +56,18 @@ const Folder = () => {
         {files && files.length ? (
           files.map((file) => (
             <div className={styles.fileCard} key={file.id}>
-              <Link to={`/${user.id}/${selectedFolder.id}/${file.id}`}>
+              <div className={styles.fileCardHeaders}>
+                <button onClick={() => downloadFile(file)}>D</button>
                 <p>
                   {file.name}.{file.format}
                 </p>
+                <button onClick={() => deleteFile(file)}>X</button>
+              </div>
+              <Link to={`/${user.id}/${selectedFolder.id}/${file.id}`}>
                 <div className={styles.file} onClick={() => setSelectedFile(file)}>
                   <img src={file.url}></img>
                 </div>
               </Link>
-              <FileButtons
-                file={file}
-                user={user}
-                selectedFolder={selectedFolder}
-                setFiles={setFiles}
-              />
             </div>
           ))
         ) : (
